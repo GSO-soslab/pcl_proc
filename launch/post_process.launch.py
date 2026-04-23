051861b67b5d3a8def94e4adb52a927a7b241fa7
tjsos <tony.jacob@uri.edu>
2026-04-23 15:44:25 -0400

    path

--- launch/post_process.launch.py
+++ launch/post_process.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
 
 
     path = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('alpha_rise_bringup'), 'launch','bringup_path.launch.py')]),
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('alpha_rise_bringup'), 'launch','bringup_path_gen.launch.py')]),
         launch_arguments={
             'robot_name': 'alpha_rise',
             'description_delay': '0.0',
@@ -94,20 +94,22 @@ def generate_launch_description():
     # bag_file_path = '/home/tony/auv_ws/bags/whale_rock/whale_rock_10_10/rosbag2_2025_10_10-18_09_27/rosbag2_2025_10_10-18_09_28'
     
     #Whale Rock 12/5
-    # bag_file_path = '/home/tony/auv_ws/bags/whale_rock/whale_rock_12_05_25/rosbag2_2025_12_05-15_29_36/rosbag2_2025_12_05-15_29_38'
-    # bag_file_path = '/home/tony/auv_ws/bags/whale_rock/whale_rock_12_05_25/rosbag2_2025_12_05-15_57_34/rosbag2_2025_12_05-15_57_35'
-    # bag_file_path = '/home/tony/auv_ws/bags/whale_rock/whale_rock_12_05_25/rosbag2_2025_12_05-16_09_45/rosbag2_2025_12_05-16_09_47'
-    # bag_file_path = '/home/tony/auv_ws/bags/whale_rock/whale_rock_12_05_25/rosbag2_2025_12_05-16_26_08/rosbag2_2025_12_05-16_26_09'
-    # bag_file_path = '/home/tony/auv_ws/bags/whale_rock/whale_rock_12_05_25/rosbag2_2025_12_05-16_43_41/rosbag2_2025_12_05-16_43_42'
-    # bag_file_path = '/home/tony/auv_ws/bags/whale_rock/whale_rock_12_05_25/rosbag2_2025_12_05-16_54_58/rosbag2_2025_12_05-16_55_00'
-    # bag_file_path = '/home/tony/auv_ws/bags/whale_rock/whale_rock_12_05_25/rosbag2_2025_12_05-17_09_41/rosbag2_2025_12_05-17_09_42'
-    # bag_file_path = '/home/tony/auv_ws/bags/whale_rock/whale_rock_12_05_25/rosbag2_2025_12_05-17_22_29/rosbag2_2025_12_05-17_22_30'
-    # bag_file_path = '/home/tony/auv_ws/bags/whale_rock/whale_rock_12_05_25/rosbag2_2025_12_05-17_35_12/rosbag2_2025_12_05-17_35_13'
+    # bag_file_path = '/media/tony/Vault/2025_12_05_Whale_Rock/rosbag2_2025_12_05-15_29_36/rosbag2_2025_12_05-15_29_38'
+    # bag_file_path = '/media/tony/Vault/2025_12_05_Whale_Rock/rosbag2_2025_12_05-15_43_49/rosbag2_2025_12_05-15_43_50'
+    # bag_file_path = '/media/tony/Vault/2025_12_05_Whale_Rock/rosbag2_2025_12_05-15_56_48/rosbag2_2025_12_05-15_56_49'
+    # bag_file_path = '/media/tony/Vault/2025_12_05_Whale_Rock/rosbag2_2025_12_05-15_57_34/rosbag2_2025_12_05-15_57_35'
+    # bag_file_path = '/media/tony/Vault/2025_12_05_Whale_Rock/rosbag2_2025_12_05-16_09_45/rosbag2_2025_12_05-16_09_47'
+    # bag_file_path = '/media/tony/Vault/2025_12_05_Whale_Rock/rosbag2_2025_12_05-16_26_08/rosbag2_2025_12_05-16_26_09'
+    # bag_file_path = '/media/tony/Vault/2025_12_05_Whale_Rock/rosbag2_2025_12_05-16_43_41/rosbag2_2025_12_05-16_43_42'
+    bag_file_path = '/media/tony/Vault/2025_12_05_Whale_Rock/rosbag2_2025_12_05-16_54_58/rosbag2_2025_12_05-16_55_00'
+    # bag_file_path = '/media/tony/Vault/2025_12_05_Whale_Rock/rosbag2_2025_12_05-17_09_41/rosbag2_2025_12_05-17_09_42'
+    # bag_file_path = '/media/tony/Vault/2025_12_05_Whale_Rock/rosbag2_2025_12_05-17_22_29/rosbag2_2025_12_05-17_22_30'
+    # bag_file_path = '/media/tony/Vault/2025_12_05_Whale_Rock/rosbag2_2025_12_05-17_35_12/rosbag2_2025_12_05-17_35_13'
     
     # bag_file_path = '/home/tony/auv_ws/bags/mbes_testing_allen_harbor_3_13_26/rosbag2_2026c_03_13-17_21_16/rosbag2_2026_03_13-17_21_16'
     # bag_file_path = '/home/tony/auv_ws/bags/mbes_testing_allen_harbor_3_13_26/rosbag2_2026_03_13-17_34_47/rosbag2_2026_03_13-17_34_48'
 
-    bag_file_path = '/media/tony/Vault/2026_04_09_Whale_rock/rosbag2_2026_04_09-18_31_27/rosbag2_2026_04_09-18_31_28'
+    # bag_file_path = '/media/tony/Vault/2026_04_09_Whale_rock/rosbag2_2026_04_09-18_31_27/rosbag2_2026_04_09-18_31_28'
 
     bag_play = ExecuteProcess(
         cmd=[
